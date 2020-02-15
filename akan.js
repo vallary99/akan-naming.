bc40979b65ab0dc3a643bb20303c10ae4b6d6673
Your Name <you@example.com>
2020-02-15 12:47:58 +0300

    Add an array and declare variables

--- akan.js
+++ akan.js
@@ -1,2 +1,20 @@
 var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleidentifier;
+var femaleidentifier;
+var maleresult, femaleresult;
+var dd, mm, yy;
+
+
+function getInputValue(){
+    var day= newDay(document.getElementById("date").value);
+    dd= day.getDay();
+    mm = day.getMonth();
+    yy = day.getFullYear();
+}
+
+/*function validateInput(){
+    alert(dd);
+    alert(mm);
+    alert(yy);
+}*/
